b1d983b445d566c4e536ad654613f5985badbc1f
Sumaer Jolly <sumaerjolly@gmail.com>
2019-11-13 16:59:46 +0530

    update game to display any random colors vs hard coded colors

--- colors.js
+++ colors.js
@@ -1,15 +1,9 @@
-var colors = [
-    "rgb(255, 0 , 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0 , 255)"
-];
+var colors = generateRandomColors(6);
 var pickedColor = pickColor();
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1")
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,6 +19,7 @@ for(var i = 0; i < squares.length ; i++){
        if(clickedColor === pickedColor){
         messageDisplay.textContent = "Correct!";
         changeColors(pickedColor);
+        h1.style.backgroundColor = clickedColor;
        }
        else{
           this.style.backgroundColor = "#232323";
@@ -43,4 +38,22 @@ function changeColors(color){
 function pickColor(){
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num){
+    var arr = [];
+    for(var i = 0; i < num ;i++) {
+        arr.push(randomColor());
+    }
+
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+    
 }
